01c09524b80f080278d1a6c031d893fa5633b674
masa <masatoshi.sboard@gmail.com>
2020-11-17 00:13:15 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,24 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
     }
+    let mut res = n;
+    for i in 0..=n {
+        let mut cc = 0;
+        let mut t = i;
+        while t > 0 {
+            cc += t % 6;
+            t /= 6;
+        }
+        t = n - i;
+        while t > 0 {
+            cc += t % 9;
+            t /= 9;
+        }
+        if res > cc {
+            res = cc;
+        }
+    }
+    println!("{}", res);
 }
